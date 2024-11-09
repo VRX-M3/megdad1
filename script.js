724ff2aeb02dc20f441ae85fac27ef496eae8be0
mahmoud megdad <mahmoudmegdad3@gmail.com>
2024-11-09 02:56:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+function redirectToDownload() {
+    // Redirect to the download link for the app
+    window.location.href = 'https://www.mediafire.com/file/682k2msgzqqacml/com.si.azkar_1.0.apk/file';
+}
